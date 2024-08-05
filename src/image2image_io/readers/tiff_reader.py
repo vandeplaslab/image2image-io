fc791b60dba5c5a029ebbe81e5b282dbd66396b5
lukasz-migas <lukas.migas@yahoo.com>
2024-08-05 14:06:03 +0100

    Update tiff_reader.py

--- src/image2image_io/readers/tiff_reader.py
+++ src/image2image_io/readers/tiff_reader.py
@@ -56,7 +56,7 @@ class TiffImageReader(BaseReader):
         self._channel_names = self._get_channel_names()
         self._channel_colors = None
         logger.trace(
-            f"{path}: RGB={self.is_rgb}; dims={self.im_dims}; px={self.resolution}; n_ch={len(self._channel_names)}"
+            f"{path}: RGB={self.is_rgb}; dims={self.im_dims}; px={self.resolution:.3f}; n_ch={len(self._channel_names)}"
         )
         init_pyramid = init_pyramid if init_pyramid is not None else CONFIG.init_pyramid
         if init_pyramid:
