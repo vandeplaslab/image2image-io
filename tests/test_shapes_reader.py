6172b5f7f9475c11939d11cf7ee6c76852cf731f
Lukasz Migas <lukas.migas@yahoo.com>
2025-04-27 21:32:19 +0100

    Update test_shapes_reader.py

--- tests/test_shapes_reader.py
+++ tests/test_shapes_reader.py
@@ -10,9 +10,11 @@ from image2image_io.readers.shapes_reader import read_shapes_from_df
 def test_read_points_from_df(columns):
     x_col, y_col, cell_col = columns
     df = pd.DataFrame({x_col: [1, 2, 3], y_col: [4, 5, 6], cell_col: ["a", "a", "a"]})
-    shapes_geojson, shapes_data = read_shapes_from_df(df)
-    assert len(shapes_data) == 1, "Expected to read points from DataFrame."
+    shapes_geojson, shapes_data, is_points = read_shapes_from_df(df)
+    assert len(shapes_data) == 1, "Expected to read shapes from DataFrame."
+    assert not is_points, "Expected to read shapes from DataFrame."
 
     df = pd.DataFrame({x_col: [1, 2, 3], y_col: [4, 5, 6], cell_col: ["a", "b", "c"]})
-    shapes_geojson, shapes_data = read_shapes_from_df(df)
-    assert len(shapes_data) == 3, "Expected to read points from DataFrame."
+    shapes_geojson, shapes_data, is_points = read_shapes_from_df(df)
+    assert len(shapes_data) == 3, "Expected to read shapes from DataFrame."
+    assert not is_points, "Expected to read shapes from DataFrame."
