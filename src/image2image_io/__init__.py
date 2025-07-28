e355cb12586f4da851141c3d3c1d2e847bf0514d
Lukasz Migas <lukas.migas@yahoo.com>
2025-07-28 17:03:45 +0100

    Update __init__.py

--- src/image2image_io/__init__.py
+++ src/image2image_io/__init__.py
@@ -2,10 +2,6 @@
 
 from importlib.metadata import PackageNotFoundError, version
 
-try:
-    __version__ = version("image2image-io")
-except PackageNotFoundError:
-    __version__ = "uninstalled"
-
+__version__ = "0.1.14"
 __author__ = "Lukasz G. Migas"
 __email__ = "lukas.migas@yahoo.com"
