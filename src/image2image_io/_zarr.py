b006718d0f55f01cc23bd34018045ef58cc59ec4
Lukasz Migas <lukas.migas@yahoo.com>
2025-11-19 14:52:01 +0000

    Update _zarr.py

--- src/image2image_io/_zarr.py
+++ src/image2image_io/_zarr.py
@@ -5,4 +5,7 @@ from zarr import Array
 try:
     from zarr.hierarchy import Group
 except (AttributeError, ModuleNotFoundError):
-    from zarr.core import Group
+    try:
+        from zarr.core.group import Group
+    except (AttributeError, ModuleNotFoundError):
+        from zarr.group import Group
